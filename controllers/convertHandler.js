ff3c296ebee73af0cbaaf64c892ec041139695af
Glitch (prairie-coyote) <none>
2018-12-20 02:56:31 +0000

    📇🌔 Checkpoint ./controllers/convertHandler.js:2177980/1392

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -8,29 +8,62 @@
 
 function ConvertHandler() {
   
-  this.getNum = function(input) {
-    var result;
-    
-    return result;
-  };
+  let regex = /([A-Za-z]+)/;
   
-  this.getUnit = function(input) {
-    var result;
+  this.getNum = (input) => {
+    let result = input.split(regex)[0];
+    if (result == "") { // default value is 1, per instructions
+      result = 1;
+    };
     
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
-  };
+    console.log("resultlength", result.length);
+    console.log("getNum: ", result);
 
-  this.spellOutUnit = function(unit) {
-    var result;
-    
+    return result || "invalid number"; // check if this does what you think it will
+  };
+  
+  this.getUnit = (input) => {
+    let result;
+    if (input.split(regex)[1]) { // if there's a unit included
+      result = input.split(regex)[1].toLowerCase();
+    }
+    else {
+      result = "invalid unit";
+    }
+        
+    console.log("getUnit: ", result);
     return result;
   };
+  
+  this.getReturnUnit = (initUnit) => {
+    let result;
+    
+    switch(initUnit) {
+    case "gal":
+      result = "l";
+      break;
+    case "l":
+      result = "gal";
+      break;
+    case "lbs":
+      result = "kg";
+      break;
+    case "kg":
+      result = "lbs";
+      break;
+    case "mi":
+      result = "km";
+      break;
+    case "km":
+      result = "mi";
+      break;
+    default:
+      result = "invalid unit";
+  }
+    return result;
+  };
+  
+  
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
